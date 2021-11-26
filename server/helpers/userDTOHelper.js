4a99d1e43cf83cb0f92d63fa0f7798268ea8af20
Victor611 <sorokin.victor.v@gmail.com>
2021-11-26 11:51:35 +0200

    second commit

--- server/helpers/userDTOHelper.js
+++
@@ -1,6 +0,0 @@
-module.exports.UserEmailDTO = (model) => {
-        this.email = model.email;
-        this.id = model.userId;
-        this.isActivated = model.isActivated;
-        return this;
-}
